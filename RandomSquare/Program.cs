5979efec669f96de34dfc5a3bd9474c0952180a7
John Fleming <johndeclanfleming@gmail.com>
2020-08-22 12:52:13 -0500

    Add project files.

---
+++ RandomSquare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumbers = new List<int>();
+            
+            for (int i = 0; i <= 20; i++)
+            {
+                randomNumbers.Add(new Random().Next(1, 50));
+            }
+
+            var squaredNumbers = randomNumbers.Select(number => number * number);
+
+            var onlyEvenSquaredNumbers = squaredNumbers.Where(number => number % 2 == 0);
+
+            foreach (var x in onlyEvenSquaredNumbers)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
